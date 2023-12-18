253a43bd19b7d3d5998d94fb478ca2f1f8eca310
webDeveloper42 <rfriashobby@gmail.com>
2023-12-18 15:26:36 -0500

    properly finishided grid creation and display

--- battleship_PartTwo.js
+++ battleship_PartTwo.js
@@ -1,29 +1,38 @@
 let rs = require("readline-sync");
-
+let theGrid = [];
+let alphabet = ["A","B","C","D","E","F","G","H","I","J"];
 function game() {
-  function start(){
-
-  };
-  function generateRandom(){
-
-  };
-  function guessing(){
-
-  };
-  function replayOrExit(){
-    let againOrQuit = rs.keyInYNStrict(
-      "You have destroyed all battleships. Would you like to play again?"
-    );
-    if (againOrQuit) {
-      game();
+  function start() {
+    rs.keyIn("Press any key to start the game ", { limit: "$<a-z>" });
+    function createGrid(rows = 10,cols = 10){
+      for (let i = 0; i < rows; i++){
+        let row = [];
+        theGrid.push(row);
+        
+        for(let k = 0; k < cols; k ++){
+          let col = [];
+          col.push(`${(alphabet[i]) + (k + 1)}`);
+          row.push(col);
+          col.join(" ");
+        }
+      }
+    }
+    createGrid();
+    for(let i = 0; i < theGrid.length; i++){
+      let gridToDisplay = theGrid[i];
+      let strings = gridToDisplay.join(" ");
+      console.log(strings);
     }
   }
+
+  function backgroundRuns(){
+
+  }
   //CheatSheet
-  
 
   //GameFunction order
-  // start();
-  //generateRandom();
+  start();
+  // backgroundRuns();
   //guessing();
   // replayOrExit();
 }
